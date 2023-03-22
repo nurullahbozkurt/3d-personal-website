33761cdb132b474f6a58d0a5a7387225cb0c23bd
nurullahbozkurt <nurullahbozkurtt@gmail.com>
2023-03-22 12:42:54 +0300

    wip

---
+++ src/constants/index.js
@@ -0,0 +1,262 @@
+import {
+  mobile,
+  typeScript,
+  backend,
+  creator,
+  web,
+  nodeJS,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  reactSVG,
+  reactNative,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "React.JS",
+    company_name: "Frontend",
+    icon: reactSVG,
+    iconBg: "#383E56",
+
+    points: [
+      "I use ReactJS to develop all web applications",
+      "I use NextJS library to develop Server Side Rendering projects and to make the best SEO optimization.",
+      "I use helper libraries like react-query and useSwr for the best data fetching operations.",
+      "I use Socket.io to develop real time projects.",
+      "I use threeJS to embed 3D visuals in the project interface.",
+    ],
+  },
+  {
+    title: "React Native",
+    company_name: "Mobile App",
+    icon: reactNative,
+    iconBg: "#E6DEDD",
+    points: [
+      "I develop mobile applications for Android and iOS platforms.",
+      "I am using React Native while developing these mobile apps.",
+      "I'm still improving myself to develop the best mobile apps.",
+    ],
+  },
+  {
+    title: "Node.JS",
+    company_name: "Backend",
+    icon: nodeJS,
+    iconBg: "#383E56",
+    points: [
+      "While developing applications on the backend, I use the MongoDB database to use the data in the best way.",
+      "I am using Express.js to handle HTTP requests and responses while working on Node.js.",
+      " I use common 3rd party applications such as sendgrid, stripe, paypal, agenda, winston to meet the payment and other possible needs of customers.",
+    ],
+  },
+  {
+    title: "TypeScript",
+    company_name: "Frontend & Backend",
+    icon: typeScript,
+    iconBg: "#E6DEDD",
+    points: [
+      "  I use `typescript` in React and NodeJS projects to get rid of potential errors and to speed up and simplify the development process.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
